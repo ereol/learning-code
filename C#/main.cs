ba8b24a9cdd22d31a01ee96abd24e2b044a9ae7e
Morrison <morrison0667@gmail.com>
2022-06-14 04:31:21 +1000

    14.06.2022

--- C#/main.cs
+++ C#/main.cs
@@ -6,8 +6,15 @@ namespace main
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello Mum");
-            
+            Console.WriteLine("Hello Mum\n");
+            Console.WriteLine("Please enter a whole number from 1-100: ");
+
+            int number = Convert.ToInt32(Console.ReadLine());
+            int computerNumber = random.IntegerValue;
+            Console.WriteLine(computerNumber);
+
+
+
         }
     }
 }
