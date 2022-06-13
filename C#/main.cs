f91f14494fc05a1a2c4bf1e55bcc132fbf58347d
Morrison <morrison0667@gmail.com>
2022-06-14 03:52:21 +1000

    14.06.2022

---
+++ C#/main.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace main
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Mum");
+            
+        }
+    }
+}
