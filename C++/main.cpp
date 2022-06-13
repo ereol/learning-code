dc176a035c05e453f0a7c70d2cec0b05773ca151
Morrison <morrison0667@gmail.com>
2022-06-14 03:45:55 +1000

    Start

---
+++ C++/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+int main()
+{ 
+    //Variables & Console Print
+    string characterName = "John";
+    int characterAge = 70;
+
+    cout << "Hello world!" << endl;
+     cout << "    /|" << endl;
+     cout << "   / |" << endl;
+     cout << "  /  |" << endl;
+     cout << " /   |" << endl;
+     cout << "/____|" << endl;
+        cout << "" << endl; 
+    cout << "There once was a man named John" << endl;
+    cout << "He was 70 years old" << endl;
+    cout << "He liked the name John" << endl;
+    cout << "But he did not like being 70" << endl;
+        cout << "" << endl; 
+    cout << "There once was a man named " << characterName << endl;
+    cout << "He was " << characterAge << " years old" << endl;
+    characterName = "Mike";
+    cout << "He liked the name " << characterName << endl;
+    cout << "But he did not like being " << characterAge << endl;
+
+
+
+    return 0;
+
+} 
--- C++/readme.txt
+++ C++/readme.txt
@@ -1,1 +1,3 @@
 I plan to learn this language soon!
+
+Started learning on 2022/06/14
