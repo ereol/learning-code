2a14724a898a1f910ff04500371c091ea6675b5a
Morrison <morrison0667@gmail.com>
2022-06-14 21:26:39 +1000

    14.06.2022

--- C#/main.cs
+++ C#/main.cs
@@ -1,7 +1,7 @@
 using System;
-using Math;
+using System.Linq;
 
-namespace main
+namespace csharp_tutorial
 {
     class MainClass
     {
@@ -11,16 +11,28 @@ namespace main
             Console.WriteLine("Hello Mum\n");
             Console.Write("Please enter a whole number from 1-100: ");
 
-            int number = Convert.ToInt32(Console.ReadLine());
+            //int number = Convert.ToInt32(Console.ReadLine());
+
+            int number;
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out number))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.Write("\nPlease enter a proper whole number: ");
+                }
+            }
             int computerNumber = rnd.Next(1, 100);
+
             Console.WriteLine("");
 
             if (computerNumber == number)
             {
                 Console.WriteLine("You are Correct");
             }
-
-
 
             else if (computerNumber < number)
             {
@@ -39,10 +51,75 @@ namespace main
             {
                 Console.WriteLine("Invalid Response");
             }
-            Console.WriteLine("\nIt was " + computerNumber);
-            
+            Console.Write("\nIt was " + computerNumber + "\nPress Enter to continue: ");
 
+            while (Console.ReadKey().Key != ConsoleKey.Enter);
+            { }
 
+            Console.Clear();
+
+            //Trying to learn Binary Search
+
+            Console.WriteLine("Now lets have the computer guess your code in 7 tries.\nPlease enter a number: ");
+
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out number))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.Write("\nPlease enter a proper whole number: ");
+                }
+            }
+
+            computerNumber = Convert.ToInt32(rnd.Next(1, 100));
+            int tries = 0;
+            string triesNumber = "try";
+            do
+            {
+                if (computerNumber == number)
+                {
+                    Console.WriteLine("The computer guessed correct");
+                    break;
+                }
+
+                else if (computerNumber > number)
+                {
+                    Console.WriteLine("The computer guessed too high");
+                    computerNumber /= 2;
+                    tries++;
+                }
+
+                else if (computerNumber < number)
+                {
+                    Console.WriteLine("The computer guessed too low");
+                    computerNumber = computerNumber/2 + computerNumber;
+                    tries++;
+                }
+                else if (number < 100 || number > 0)
+                {
+                    Console.WriteLine("Invalid Response");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Response");
+                }
+            }
+            while (computerNumber != number);
+
+            if(tries >= 2)
+            {
+                triesNumber = " tries.";
+            }
+            else
+            {
+                triesNumber = " try.";
+            }
+            Console.WriteLine("\nThe computer found your number.\nIt was " + computerNumber +"\nIt took "+ tries + triesNumber + "\n\nThanks for playing!\nPress Enter to continue: ");
+            while (Console.ReadKey().Key != ConsoleKey.Enter);
+            { }
 
         }
     }
