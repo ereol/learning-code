dad174b269fb5b0f8ca03c74d3191cd0952575b9
ereol <morrison0667@gmail.com>
2022-06-18 04:50:56 +1000

    18 06 2022

---
+++ C#/Hangman/hangman.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+string f = " ____";
+string e = "|";
+string d = "    | ";
+string c = "\n|    O";
+string b = "\n|   /|\\";
+string a = "\n|   / \\";
+string g = "\n ______\n";
+
+Console.WriteLine(f);
+Console.Write(e);
+Console.Write(d);
+Console.Write(c);
+Console.Write(b);
+Console.Write(a);
+Console.Write(g);
+
+
+
+for(lengthWord = 0, lengthWord < wordLength, )
+
+
+
+
+
