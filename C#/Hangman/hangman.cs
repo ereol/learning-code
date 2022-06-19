d34f7aea950a0523ec43361bf15369f65e409641
ereol <morrison0667@gmail.com>
2022-06-20 06:56:03 +1000

    20 06 2022

--- C#/Hangman/hangman.cs
+++ C#/Hangman/hangman.cs
@@ -8,7 +8,6 @@ string c = "\n|    O";
 string b = "\n|   /|\\";
 string a = "\n|   / \\";
 string g = "\n ______\n";
-
 // Console.WriteLine(f);
 // Console.Write(e);
 // Console.Write(d);
@@ -17,23 +16,86 @@ string g = "\n ______\n";
 // Console.Write(a);
 // Console.Write(g);
 
-
+/*
+welcome screen
+*/
 
 string word;
-int option = 0;
-
-Console.Write("Welcome to the Hangman Game");
+Console.Write("Welcome to the Hangman Game. ");
 System.Threading.Thread.Sleep(5000);
 Console.Clear();
-Console.Write("Please enter a word: ")
+Console.Write("Please enter a word: ");
 word = Console.ReadLine();
+Console.Clear();
+//cleared console
+int wordLength = word.Length;
+    //string lineLength = String.Concat(Enumerable.Repeat("_ ", wordLength));
+string wordLower = word.ToLower();
+ConsoleKeyInfo guessWord = new ConsoleKeyInfo();
+int tries = 0;
+int success = 0;
+string[] result = new string[word.Length];
+string[] alphabet = new string[word.Length+10];
+Console.WriteLine(f);
+Console.Write(e);
+Console.Write(d);
+Console.Write(c);
+Console.Write(b);
+Console.Write(a);
+Console.Write(g+"\n\nLets guess the word!\n");
+Console.WriteLine("\n\n"+lineLength+"\n\n");
+Console.WriteLine("\nPlease enter a character: ");
+//real game
+while(true)
+{
+    guessWord = Console.ReadKey();
+    if(success == word.Length) //if amount of success attempts matches word length, exit game
+    {
+        break;
+    }
+    else if(word.IndexOf(guessWord.KeyChar) != -1) //if the character is in the word, it gives an index not equal to -1
+    {
+        gameStatement;
+        success++;
+    }
+    else //fails
+    {
+        gameStatement;
+        tries++;
+
+    }
+    if( tries == 7)
+    {
+        Console.WriteLine("You have failed!\nGoodbye!");
+        System.Threading.Thread.Sleep(5000);
+        return 0;
+    }
+}
+return 0;   
 
 
-// for(lengthWord = 0, lengthWord < word.Length, lengthWord++);
-// {
 
-// }
-
-
-
-
+public string gameStatement()
+{
+    switch(tries)
+    {
+    case 1:  
+    return Console.WriteLine("\n"+e+d+c+b+a+g+"\n\n"+lineLength+"\n\n"+"Please enter a character: ");
+    break;
+    case 2:
+    return Console.WriteLine("\n"+d+c+b+a+g+"\n\n"+lineLength+"\n\n"+"Please enter a character: ");
+    break;
+    case 3:
+    return Console.WriteLine("\n"+c+b+a+g+"\n\n"+lineLength+"\n\n"+"Please enter a character: ");
+    break;
+    case 4:
+    return Console.WriteLine("\n"+b+a+g+"\n\n"+lineLength+"\n\n"+"Please enter a character: ");
+    break;
+    case 5:
+    return Console.WriteLine("\n"+a+g+"\n\n"+lineLength+"\n\n"+"Please enter a character: ");
+    break;
+    case 6:
+    return Console.WriteLine("\n"+g+"\n\n"+lineLength+"\n\n"+"Please enter a character: ");
+    break;
+    }
+}
