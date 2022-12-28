a1e603377c1db5ac733582253d327f69d98ec0ad
ereol <morrison0667@gmail.com>
2022-12-28 22:13:02 +0700

    28.12.22

---
+++ Rust/hello_cargo/target/debug/deps/hello_cargo-1690ef88d930c8b3.d
@@ -0,0 +1,5 @@
+/Users/morro/Documents/Personal/Coding/learning-code/Rust/hello_cargo/target/debug/deps/hello_cargo-1690ef88d930c8b3: src/main.rs
+
+/Users/morro/Documents/Personal/Coding/learning-code/Rust/hello_cargo/target/debug/deps/hello_cargo-1690ef88d930c8b3.d: src/main.rs
+
+src/main.rs:
---
+++ Rust/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/morro/Documents/Personal/Coding/learning-code/Rust/hello_cargo/target/debug/hello_cargo: /Users/morro/Documents/Personal/Coding/learning-code/Rust/hello_cargo/src/main.rs
