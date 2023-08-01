c2bbfb7007266db8ef50741a4d472dbb32ca12bf
ereik <57711849+ereol@users.noreply.github.com>
2023-08-02 02:43:00 +1000

    02-08-2023

--- Python/ThrustRequiredTutorial2.py
+++ Python/ThrustRequiredTutorial2.py
@@ -18,6 +18,8 @@ CD_array = []
 LD_array = []
 Tr_array = []
 V_array = []
+Pr_array = []
+Pa_array = []
 
 #functions to find Cl Cd
 def findCL(w,p,v,s):
@@ -31,20 +33,29 @@ def findCd(cd0, cl, e, ar):
 #looping to get all Tr for each velocity m/s 100->320
 #0 MSL
 print("[-- MSL = 0m --]")
-for V in range(100, 340, 20):
+for V in range(20, 340, 20):
     Cl = findCL(W,p0, V, S)
     Cd = findCd(Cd0, Cl, e, AR)
     LD = Cl/Cd
     Tr = W/LD
+    Pr = (Tr*V)/1000
+    Pa = (2*T*100)/1000
+
     Cl =round(Cl, 2)
     Cd = round(Cd, 2)
     LD = round(LD, 2)
     Tr = round(Tr, 2)
+    Pr = round(Pr, 2)
+    Pa = round(Pa, 2)
+
     CL_array.append(Cl)
     CD_array.append(Cd)
     LD_array.append(LD)
     Tr_array.append(Tr)
     V_array.append(V)
+    Pr_array.append(Pr)
+    Pa_array.append(Pa)
+
     print(text.format(V, Cl, Cd, LD, Tr, row))
     row = row + 1
 print(text2.format(CL_array, CD_array, LD_array, Tr_array))
@@ -54,12 +65,18 @@ print("\n [--------------]")
 #ypoints = np.array(Tr_array)
 
 fig = plt.figure()
-ax1 = fig.add_subplot(1, 2, 1)
+ax1 = fig.add_subplot(2, 2, 1)
 ax1.set_title("V vs Tr @ 0m MSL")
 #ax1.plot(xpoints, ypoints, label = "0m MSL", marker = 'o')
 ax1.plot(V_array, Tr_array, label = "0m MSL", marker = 'o')
-ax1.set_xlabel("Freestream Velocity, V")
-ax1.set_ylabel("Thrust Required, Tr")
+ax1.set_xlabel("Freestream Velocity, V\n (m/s)")
+ax1.set_ylabel("Thrust Required, Tr\n (N)")
+
+ax3 = fig.add_subplot(2, 2, 3)
+ax3.set_title("V vs Power Required @ 0m MSL")
+ax3.plot(V_array, Pr_array, marker = 'o')
+ax3.set_xlabel('Freestream Velocity, V\n (m/s)')
+ax3.set_ylabel('Power Required, kW')
 
 
 #reset
@@ -70,23 +87,33 @@ CD_array = []
 LD_array = []
 Tr_array = []
 V_array = []
+Pr_array = []
+Pa_array = []
 
 #5000m MSL
 print("[-- MSL = 5000m --]")
-for V in range(100, 340, 20):
+for V in range(20, 340, 20):
     Cl = findCL(W,p1, V, S)
     Cd = findCd(Cd0, Cl, e, AR)
     LD = Cl/Cd
     Tr = W/LD
+    Pr = (Tr*V)/1000
+    Pa = (2*T*100)/1000
     Cl =round(Cl, 2)
     Cd = round(Cd, 2)
     LD = round(LD, 2)
     Tr = round(Tr, 2)
+    Pr = round(Pr, 2)
+    Pa = round(Pa, 2)
+
     CL_array.append(Cl)
     CD_array.append(Cd)
     LD_array.append(LD)
     Tr_array.append(Tr)
     V_array.append(V)
+    Pr_array.append(Pr)
+    Pa_array.append(Pa)
+
     print(text.format(V, Cl, Cd, LD, Tr, row))
     row = row+1
 print(text2.format(CL_array, CD_array, LD_array, Tr_array))
@@ -94,13 +121,18 @@ print("\n [--------------]")
 
 #x1points = np.array(V_array)
 #y2points = np.array(Tr_array)
-ax2 = fig.add_subplot(1, 2, 2)
+ax2 = fig.add_subplot(2, 2, 2)
 #ax2.plot(x1points, y2points, label = "5000m MSL", marker = 'o')
 ax2.plot(V_array, Tr_array, label = "5000m MSL", marker = 'o')
 ax2.set_title("V vs Tr @ 5000m MSL")
 ax2.set_xlabel("Freestream Velocity, V")
 ax2.set_ylabel("Thrust Required, Tr")
 
+ax4 = fig.add_subplot(2, 2, 4)
+ax4.set_title("V vs Power Required @ 5000m MSL")
+ax4.plot(V_array, Pr_array, marker = 'o')
+ax4.set_xlabel('Freestream Velocity, V\n (m/s)')
+ax4.set_ylabel('Power Required, kW')
 
 plt.show()
 
