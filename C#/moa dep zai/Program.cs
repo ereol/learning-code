96e3812f407597f4eaa2a0a8b3f5414879be63d1
ereik <57711849+ereol@users.noreply.github.com>
2023-12-29 22:10:17 +1100

    29-12-23

--- C#/moa dep zai/Program.cs
+++ C#/moa dep zai/Program.cs
@@ -1,38 +1,120 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System.Collections;
+﻿// // See https://aka.ms/new-console-template for more information
+// using System.Collections;
 
-Console.Write("Working hours = ");
-Int32 w = Int32.Parse(Console.ReadLine());
+// Console.Write("Working hours = ");
+// Int32 w = Int32.Parse(Console.ReadLine());
 
-Console.Write("Unit price = ");
-Int32 u = Int32.Parse(Console.ReadLine());
+// Console.Write("Unit price = ");
+// Int32 u = Int32.Parse(Console.ReadLine());
 
-Console.WriteLine("Your salary = " + (w*u));
+// Console.WriteLine("Your salary = " + (w*u));
 
-Console.Write("Enter to continue: ");
-Console.ReadKey();
-// -----------------------------------------------
-Console.Clear();
+// Console.Write("Enter to continue: ");
+// Console.ReadKey();
+// // -----------------------------------------------
+// Console.Clear();
 
-Console.Write("Amount of money = ");
-Int32 Amt = Int32.Parse(Console.ReadLine());
-Console.WriteLine("");
+// Console.Write("Amount of money = ");
+// Int32 Amt = Int32.Parse(Console.ReadLine());
+// Console.WriteLine("");
 
-int oneDollar, twoDollars, fiveDollars, tenDollars, twentyDollars, fiftyDollars, hundredDollars;
+// int oneDollar, twoDollars, fiveDollars, tenDollars, twentyDollars, fiftyDollars, hundredDollars;
 
-hundredDollars = Amt / 100;
-Amt = Amt - hundredDollars*100;
-fiftyDollars = Amt / 50;
-Amt = Amt - fiftyDollars*50;
-twentyDollars = Amt / 20;
-Amt = Amt - twentyDollars*20;
-tenDollars = Amt / 10;
-Amt = Amt - tenDollars*10;
-fiveDollars = Amt / 5;
-Amt = Amt - fiveDollars*5;
-twoDollars = Amt / 2;
-Amt = Amt - twoDollars*2;
-oneDollar = Amt / 1;
+// hundredDollars = Amt / 100;
+// Amt = Amt - hundredDollars*100;
+// fiftyDollars = Amt / 50;
+// Amt = Amt - fiftyDollars*50;
+// twentyDollars = Amt / 20;
+// Amt = Amt - twentyDollars*20;
+// tenDollars = Amt / 10;
+// Amt = Amt - tenDollars*10;
+// fiveDollars = Amt / 5;
+// Amt = Amt - fiveDollars*5;
+// twoDollars = Amt / 2;
+// Amt = Amt - twoDollars*2;
+// oneDollar = Amt / 1;
 
 
-Console.WriteLine("Hundred dollars = {0}\nFifty Dollars = {1}\nTwenty Dollars = {2}\nTen Dollars = {3}\nFive Dollars = {4}\nTwo Dollars = {5}\nOne Dollars = {6}", hundredDollars, fiftyDollars, twentyDollars, tenDollars, fiveDollars, twoDollars, oneDollar);
+// Console.WriteLine("Hundred dollars = {0}\nFifty Dollars = {1}\nTwenty Dollars = {2}\nTen Dollars = {3}\nFive Dollars = {4}\nTwo Dollars = {5}\nOne Dollars = {6}", hundredDollars, fiftyDollars, twentyDollars, tenDollars, fiveDollars, twoDollars, oneDollar);
+
+
+
+using System.Security.Cryptography.X509Certificates;
+
+namespace cong_2_ma_tran
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] a;
+            int[,] b;
+            Console.WriteLine("nhap m = ");
+            int m = int.Parse(Console.ReadLine());
+            Console.WriteLine("nhap n = ");
+            int n = int.Parse(Console.ReadLine());
+            a = new int[m, n];
+            Input(a);
+            Output(a);            
+            Console.WriteLine("nhap x = ");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("nhap y = ");
+            int y = int.Parse(Console.ReadLine());
+            b = new int[x,y];
+            Input(b);
+            Output(b);
+
+            Console.WriteLine("tong 2 ma tran  = ");
+            TinhTong2MaTran(a, b);
+        }
+        static void Input(int[,]x)
+        {
+            for (int i = 0; i < x.GetLength(0); i++)
+            {
+                for(int j = 0; j < x.GetLength(1); j++)
+                {
+                    Console.Write($"nhap so [{i},{j}]: ");
+                    x[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+        }
+        static void Output(int[,]x)
+        {
+            for (int i = 0; i < x.GetLength(0); i++)
+            {
+                for (int j = 0; j < x.GetLength(1); j++)
+                {
+                    Console.Write(x[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void TinhTong2MaTran(int[,]a, int[,]b)
+        {
+            int[,] x;
+            if (a.GetLength(0) != b.GetLength(0) || a.GetLength(1) != b.GetLength(1))
+            {
+                Environment.Exit(0);
+            }
+
+
+            int[,]c = new int[x.GetLength(0), x.GetLength(1)];
+            for (int i = 0;i < x.GetLength(0); i++)
+            {
+                for (int j = 0; j < x.GetLength(1);j++)
+                {
+                    c[i,j] = a[i,j] + b[i,j];                         
+                }
+            }
+            for (int i = 0; i < c.GetLength(0); i++)
+            {
+                for (int j = 0; j < c.GetLength(1);j++)
+                {
+                    Console.Write(c[i, j]+ " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
---
+++ Python/Flight Mechanics/Assignment4/8.py
@@ -0,0 +1,21 @@
+import openpyxl
+import ExcelSorter as ExS
+import FinalFunc
+
+data0 = openpyxl.load_workbook(r'D:/Desktop/Academic/2023/Semester 2/AERO - Flight Dynamics/Alpha_8_Elev_Varying/alpha_8_elev_7.xlsx')
+data1 = openpyxl.load_workbook(r'D:/Desktop/Academic/2023/Semester 2/AERO - Flight Dynamics/Alpha_8_Elev_Varying/alpha_8_elev_4.xlsx')
+data2 = openpyxl.load_workbook(r'D:/Desktop/Academic/2023/Semester 2/AERO - Flight Dynamics/Alpha_8_Elev_Varying/alpha_8_elev_0.xlsx')
+data3 = openpyxl.load_workbook(r'D:/Desktop/Academic/2023/Semester 2/AERO - Flight Dynamics/Alpha_8_Elev_Varying/alpha_8_elev_minus4.xlsx')
+data4 = openpyxl.load_workbook(r'D:/Desktop/Academic/2023/Semester 2/AERO - Flight Dynamics/Alpha_8_Elev_Varying/alpha_8_elev_minus7.xlsx')
+data5 = openpyxl.load_workbook(r'D:/Desktop/Academic/2023/Semester 2/AERO - Flight Dynamics/Alpha_8_Elev_Varying/alpha_8_elev_minus14.xlsx')
+
+note0 = data0.active
+note1 = data1.active
+note2 = data2.active
+note3 = data3.active
+note4 = data4.active
+note5 = data5.active
+
+notes = [note0, note1, note2, note3, note4, note5]
+print("[AoA = 8]\n")
+FinalFunc.Calculation(notes)
